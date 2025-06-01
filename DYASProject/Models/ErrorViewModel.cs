7b02ab56b7c1fff0c558172f01dce5c6b0542b9a
Erick L. <ecklpz@gmail.com>
2025-05-31 23:55:39 -0500

    Add project files.

---
+++ DYASProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DYASProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
